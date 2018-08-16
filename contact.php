3cddfdcd63fef868e8f62877fb5714b65ac78d7c
Shreyas Angara <shreyas00023@gmail.com>
2018-08-16 13:19:26 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,1 +1,10 @@
-
+<?php $name = $_POST['demo-name'];
+$email = $_POST['demo-email'];
+$message = $_POST['demo-message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "shreyas00023@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
